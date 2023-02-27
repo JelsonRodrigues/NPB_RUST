713f4b0aae52d9b574643f17693b6e03e5bfa2dd
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2023-02-27 16:12:05 -0300

    ADD: display timers

--- src/commom/timer.rs
+++ src/commom/timer.rs
@@ -1,5 +1,9 @@
 use std::time::Duration;
 
-pub fn show_time(duration: Duration) {
-
+pub fn show_time(duration: &Duration) {
+    let ms = duration.as_millis() % 1000;
+    let s = duration.as_secs() % 60;
+    let m = duration.as_secs() / 60 % 60;
+    let h = duration.as_secs() / 60 / 60;
+    println!("\t{h}h:{m}m:{s}s:{ms}ms"); 
 }
--- src/ep.rs
+++ src/ep.rs
@@ -1,9 +1,12 @@
 mod commom;
 
+use std::borrow::Borrow;
+
 use commom::random::Random;
+use commom::timer::show_time;
 
 // This value comes from the NASA paper https://www.nas.nasa.gov/assets/pdf/techreports/1994/rnr-94-007.pdf
-const SEED: u64 = 271_828_813;  
+const SEED: u64 = 271_828_813;
 
 fn main() {
     let mut random_source = Random::new(SEED);
@@ -11,12 +14,13 @@ fn main() {
     let passes: u64 = 1_000_000_000;
 
     let antes = std::time::Instant::now();
-    let mut sum = 0.0;
+    let mut sum = 0.0 ;
     for _ in 0..passes {
         let a = random_source.next_f64();
         sum += a;
         // println!("{:.20}", a);
     }
     let agora = std::time::Instant::now();
-    println!("Tempo {}ms sum {sum}", (agora - antes).as_millis());
+    println!("sum {sum}");
+    show_time((agora - antes).borrow());
 }
