1fa4fa38ae77cf80552096fb62de0284a1d89ebd
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2023-03-08 20:21:54 -0300

    FIX: print time

--- src/commom/timer.rs
+++ src/commom/timer.rs
@@ -5,5 +5,6 @@ pub fn show_time(duration: &Duration) {
     let s = duration.as_secs() % 60;
     let m = duration.as_secs() / 60 % 60;
     let h = duration.as_secs() / 60 / 60;
-    println!("\t{h}h:{m}m:{s}s:{ms}ms"); 
+    println!("Total time: {}s", duration.as_secs_f64());
+    println!("\t{h:02}h:{m:02}m:{s:02}s:{ms:03}ms"); 
 }
