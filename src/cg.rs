05e8bc9a21691e54afac405b6c459ac5b4818968
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2023-04-14 11:11:52 -0300

    Conjugate gradient algorithm implemented

--- src/cg.rs
+++ src/cg.rs
@@ -16,43 +16,123 @@ fn main() {
     // Values for the S class
     let n:usize = 1400;
     let iterations = 15;
-    let lambda = 10;
+    let lambda = 10.0;
     let non_zeros = 7;
 
 
     // Create the matix and the vectors
-    let mut x:Vec<f64> = Vec::new();
-    
+    let mut x:Vec<f64> = Vec::with_capacity(n);
+    let mut z:Vec<f64> = Vec::with_capacity(n);
+    let mut A: Vec<Vec<f64>> = Vec::new();
+
+    make_A(&mut A);
+
+    x.fill(1.0);
+    z.fill(0.0);
+
     // Main loop
     for i in 0..iterations {
-
-        
+        let r = conjugate_gradient(&A, &mut z, &x);
+        let zeta = lambda - 1.0 / multiply_vector_by_column(&x, &z, 1.0);
+        println!("Iteration = {i}, ||r|| = {r}, zeta = {zeta}");
+        multiply_add_self(&mut x, &z, 1.0 /  magnitude(&z));
     }
-
-
 }
 
 fn create_matrix(order:usize, non_zeros:usize){
     todo!()
 }
 
-fn conjugate_gradient(A:&mut Vec<Vec<f64>>) -> f64 {
-    todo!()
+const CONJUGATE_GRADIENT_ITERATIONS :u32 = 25;
+
+fn conjugate_gradient(A:&Vec<Vec<f64>>, z:&mut Vec<f64>, x:&Vec<f64>) -> f64 {
+    // Assert A, z and x are the same size
+    assert!(A.len() == z.len());
+    assert!(A.len() == x.len());
+    
+    /*
+    Change this section from alocating inside the function to receive the buffers
+    The allocation and releasing of these buffers can get quite expensive, because 
+    the size is huge 1400 <= x <= 9000000
+    */
+
+    // Initialize z to 0
+    z.fill(0.0);
+
+    // Initialize r to x
+    let mut r:Vec<f64> = Vec::with_capacity(x.len());
+    r.clone_from(&x);
+
+    let mut rho = multiply_vector_by_column(&r, &r, 0.0);
+
+    let mut p:Vec<f64> = Vec::with_capacity(r.len());
+    p.clone_from(&r);
+
+    // Prealocate for q
+    let mut q : Vec<f64> = Vec::with_capacity(p.len());
+    q.fill(0.0);
+
+    for _ in 1..CONJUGATE_GRADIENT_ITERATIONS {
+        // q = Ap
+        multiply_matrix_by_vector(&A, &p, &mut q);
+        // alpha = rho / (p * q)
+        let alpha = rho / multiply_vector_by_column(&p, &q, 1.0);
+        // z = z + alpha * p
+        multiply_add_self(z, &q, alpha);
+        // rho_0 = rho
+        let rho_0 = rho;
+        // r = r - alpha * q 
+        multiply_add_self(&mut r, &q, -alpha);
+        // rho = r * r
+        rho = multiply_vector_by_column(&r, &r, 1.0);
+        // beta = rho / rho_0
+        let beta = rho / rho_0;
+        // p = r + beta * p
+        add_multiply_self(&mut p, &r, beta);
+    }
+
+    // Here i'm using the r vector as a temp to the result of Az
+    // r = A * z
+    multiply_matrix_by_vector(&A, &z, &mut r);
+    // r = x - r
+    add_multiply_self(&mut r, x, -1.0);
+
+    return magnitude(&r);
 }
 
 // Return the multiplication of a * (b*scalar)
-fn multiply_vectors(a: &Vec<f64>, b:&Vec<f64>, scalar: f64) -> Vec<f64>{
+fn multiply_vector_by_column(a: &Vec<f64>, b:&Vec<f64>, scalar: f64) -> f64{
+    assert_eq!(a.len(), b.len());
+
+    let len = a.len();
+    let mut result = 0.0;
+    for i in 0..len {
+        result += a[i] * b[i] * scalar;
+    }
+
+    return  result;
+}
+
+// Calculate result = matrix * vector
+fn multiply_matrix_by_vector(matrix:&Vec<Vec<f64>>, vector:&Vec<f64>, result:&mut Vec<f64>) {
     todo!()
 }
 
-// Compute a = a + b * scalar
-// Where a, b are two vectors with the same length and scalar is a float value that is multiplied by every
-// value of b
-fn multiply_add_self(a: &mut Vec<f64>, b:&Vec<f64>, scalar:f64 ) {
+// Compute vector_a = vector_a + vector_b * scalar
+fn multiply_add_self(vector_a: &mut Vec<f64>, vector_b:&Vec<f64>, scalar:f64) {
     todo!()
 }
 
-// Calculate the magnitude of 
-fn magnitude(a:&Vec<f64>) -> f64 {
+// Compute vector_a = vector_a * scalar + vector_b
+fn add_multiply_self(vector_a: &mut Vec<f64>, vector_b:&Vec<f64>, scalar: f64) {
+    todo!()
+}
+
+// Calculate the magnitude of vector
+fn magnitude(vector:&Vec<f64>) -> f64 {
+    multiply_vector_by_column(vector, vector, 1.0).sqrt()
+}
+
+fn make_A(A:&mut Vec<Vec<f64>>) {
     todo!()
 }
